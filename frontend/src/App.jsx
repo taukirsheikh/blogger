c0ca16f684b80b4bee2a580f7cf1802141d17110
taukirsheikh <taukirsheikh2@gmail.com>
2024-04-30 22:26:38 +0545

    author slice created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Home from './pages/Home'
 import { Routes, Route } from "react-router-dom";
 import SingleBlogView from './pages/SingleBlogView'
---
+++ frontend/src/redux/authorSlice.jsx
@@ -0,0 +1,29 @@
+import {createSlice} from '@reduxjs/toolkit'
+const initialState={
+    isLoggedIn:false,
+    author:null,
+    loginDetails:[],
+    blogByAuthor:[]
+
+}
+
+const authorSlice= createSlice({
+    name: "user",
+    initialState,
+    reducers:{
+        login: (state, action) => {
+            state.author = action.payload;
+            console.log(state.author);
+            state.isLoggedIn = true;
+          },
+          logout: (state) => {
+            state.author = null;
+            state.loginDetails = [];
+            state.isLoggedIn = false;
+          },
+          fetchBlogList: (state, action) => {
+            state.blogByAuthor = action.payload
+          },
+       
+    }
+})
